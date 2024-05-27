6e9d0d1cbed50736d21ee33342fdd1172fef53cb
Edwin Lam <edwin.lam.smarthon@gmail.com>
2024-05-28 00:12:28 +0800

    Initial commit

---
+++ docs/_build/html/_sources/index.rst.txt
@@ -0,0 +1,13 @@
+.. Edwin's Intern Life documentation master file, created by
+   sphinx-quickstart on Mon May 27 22:50:46 2024.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to Edwin's Intern Life's documentation!
+===============================================
+
+.. toctree::
+   :maxdepth: 2
+   
+   FAQ
+   實際情況
---
+++ docs/conf.py
@@ -0,0 +1,46 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+from recommonmark.parser import CommonMarkParser
+from recommonmark.transform import AutoStructify
+
+project = "Edwin's Intern Life"
+copyright = '2024, edwinlamc'
+author = 'edwinlamc'
+release = '2024'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+
+source_parsers = {
+'.md': CommonMarkParser,
+}
+
+source_suffix = ['.rst', '.md']
+extensions = [
+'sphinx_markdown_tables',
+'recommonmark'
+]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# These paths are either relative to html_static_path
+# or fully qualified paths (eg. https://...)
+html_css_files = [
+'css/custom.css',
+]
